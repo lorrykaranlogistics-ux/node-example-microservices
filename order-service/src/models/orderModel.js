9ef25f042c206c875e07c5e7eac7581146d7f2a1
Sivamani <developer.growupsolutions@gmail.com>
2026-03-03 22:14:53 +0530

    order route updated

--- order-service/src/models/orderModel.js
+++ order-service/src/models/orderModel.js
@@ -13,7 +13,6 @@ const createOrder = (orderInput) => {
     billingAddress: orderInput.billingAddress || null,
     customerNote: orderInput.customerNote || null,
     couponCode: orderInput.couponCode || null,
-    priority: orderInput.priority || 'normal',
     status: 'canceled',
   };
   orders.push(order);
