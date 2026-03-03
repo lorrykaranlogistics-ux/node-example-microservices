3546681278a9c431da4f7d6321109d559ba80b36
Sivamani <developer.growupsolutions@gmail.com>
2026-03-03 22:31:01 +0530

    order route updated

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get('/orders', getOrders);
 router.get('/orders/:id', getOrder);
 router.post('/orders', create);
+router.post('/orders/:id/update', create);
 router.patch('/orders/:id/status', patchOrderStatus);
 
 module.exports = router;
